2d93995827980a4f160b35f6a4f62b40dfb6a98c
Anna Matusevich <am10tyxu@studserv.uni-leipzig.de>
2022-12-18 23:27:49 +0100

    update results group 4 and 1

--- analyzeRTS.sh
+++ analyzeRTS.sh
@@ -1,33 +1,19 @@
 function analyzeStatistics {
-	folder=$1
-	
-	echo -n "Tests: "
-	cat $1/staticTestSelection_*.json | jq ".initialcommit.initialDependencies | keys | length"
-
-	echo -n "Commits (excluding initial): "
-	cat $1/staticTestSelection_*.json | jq ".commits | keys | length"
-
-	echo -n "Not running commits: "
-	cat $1/staticTestSelection_*.json | jq ".commits" | grep "\"running\": false" | wc -l
-
-	echo -n "Commits containing a Java change: "
-	cat $1/staticTestSelection_*.json | jq ".commits[].changedClazzes | select(length > 0) | length" | wc -l
-
-	echo -n "Commits containing a measurable test case: "
-	cat $1/traceTestSelection_*.json  | jq ".commits[].testcases | select(length>0) | length" | wc -l
-}
-
-function getOverallCommits {
 	selectionFile="$project/results/staticTestSelection_*.json"
+	
 	commits=$(cat $selectionFile | jq ".commits | keys | length")
 	tests=$(cat $selectionFile | jq ".initialcommit.initialDependencies | keys | length")
 	notRunning=$(cat $selectionFile | jq ".commits" | grep "\"running\": false" | wc -l)
 	commitsWithChange=$(cat $selectionFile | jq ".commits[].changedClazzes | select(length > 0) | length" | wc -l)
 	coveredChanges=$(cat $project/results/traceTestSelection_*.json  | jq ".commits[].testcases | select(length>0) | length" | wc -l)
+	
 	runsCoverage=$(cat ${project}runCoverage-* | wc -l)
 	runsTrace=$(cat ${project}runTrace-* | wc -l)
+	runsTwice=$(cat ${project}runTwice-* | wc -l)
 		
-	echo "Tests: $tests Commits: $commits Unausführbar: $notRunning Commits mit Änderung: $commitsWithChange Abgedeckte Änderungen: $coveredChanges Trace Runs: $runsTrace Coverage Runs: $runsCoverage"
+	echo "Tests: $tests Commits: $commits notRunning commits: $notRunning"
+	echo "Commits containing a Java change: $commitsWithChange Commits containing a measurable test case: $coveredChanges"
+	echo "Trace Runs: $runsTrace TwiceExecutable Runs: $runsTwice Coverage Runs: $runsCoverage"
 	echo
 }
 
@@ -35,7 +21,6 @@ for project in ../thesis_results/*/
 do
 	echo "=============="
 	echo "$project"
-	analyzeStatistics $project/results
-	getOverallCommits	
+	analyzeStatistics	
 done
 
